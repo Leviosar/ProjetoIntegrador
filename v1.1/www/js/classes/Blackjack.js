b404ef15f6a3d0c7a6a7baa56db1bfe0c312cbb4
Leviosar <joaocampo2@hotmail.com>
2018-10-14 15:51:29 -0300

    Blackjack added

--- v1.1/www/js/classes/Blackjack.js
+++ v1.1/www/js/classes/Blackjack.js
@@ -5,6 +5,13 @@ Array.prototype.remove = function(index){
 class Blackjack{
     
     constructor(container){
+        this.closeButton = document.querySelector('button.closeButton')
+        this.bet = 1
+        this.betButtons = {
+            raise: document.querySelector('main > section > div#blackjack > div:last-child > div:first-child > span:first-child > button:first-child'),
+            lower: document.querySelector('main > section > div#blackjack > div:last-child > div:first-child > span:first-child > button:last-child')
+        }
+        this.betContainer = document.querySelector('main > section > div#blackjack > div:last-child > div:first-child > span:last-child > span.bet > p')
         this.deck = [{suit:"hearts",value:2},{suit:"hearts",value:3},{suit:"hearts",value:4},{suit:"hearts",value:5},{suit:"hearts",value:6},{suit:"hearts",value:7},{suit:"hearts",value:8},{suit:"hearts",value:9},{suit:"hearts",value:10},{suit:"hearts",value:11},{suit:"hearts",value:12},{suit:"hearts",value:13},{suit:"hearts",value:1},{suit:"diamonds",value:2},{suit:"diamonds",value:3},{suit:"diamonds",value:4},{suit:"diamonds",value:5},{suit:"diamonds",value:6},{suit:"diamonds",value:7},{suit:"diamonds",value:8},{suit:"diamonds",value:9},{suit:"diamonds",value:10},{suit:"diamonds",value:11},{suit:"diamonds",value:12},{suit:"diamonds",value:13},{suit:"diamonds",value:1},{suit:"clubs",value:2},{suit:"clubs",value:3},{suit:"clubs",value:4},{suit:"clubs",value:5},{suit:"clubs",value:6},{suit:"clubs",value:7},{suit:"clubs",value:8},{suit:"clubs",value:9},{suit:"clubs",value:10},{suit:"clubs",value:11},{suit:"clubs",value:12},{suit:"clubs",value:13},{suit:"clubs",value:1},{suit:"spades",value:2},{suit:"spades",value:3},{suit:"spades",value:4},{suit:"spades",value:5},{suit:"spades",value:6},{suit:"spades",value:7},{suit:"spades",value:8},{suit:"spades",value:9},{suit:"spades",value:10},{suit:"spades",value:11},{suit:"spades",value:12},{suit:"spades",value:13},{suit:"spades",value:1}]
         this.remaining = [{suit:"hearts",value:2},{suit:"hearts",value:3},{suit:"hearts",value:4},{suit:"hearts",value:5},{suit:"hearts",value:6},{suit:"hearts",value:7},{suit:"hearts",value:8},{suit:"hearts",value:9},{suit:"hearts",value:10},{suit:"hearts",value:11},{suit:"hearts",value:12},{suit:"hearts",value:13},{suit:"hearts",value:1},{suit:"diamonds",value:2},{suit:"diamonds",value:3},{suit:"diamonds",value:4},{suit:"diamonds",value:5},{suit:"diamonds",value:6},{suit:"diamonds",value:7},{suit:"diamonds",value:8},{suit:"diamonds",value:9},{suit:"diamonds",value:10},{suit:"diamonds",value:11},{suit:"diamonds",value:12},{suit:"diamonds",value:13},{suit:"diamonds",value:1},{suit:"clubs",value:2},{suit:"clubs",value:3},{suit:"clubs",value:4},{suit:"clubs",value:5},{suit:"clubs",value:6},{suit:"clubs",value:7},{suit:"clubs",value:8},{suit:"clubs",value:9},{suit:"clubs",value:10},{suit:"clubs",value:11},{suit:"clubs",value:12},{suit:"clubs",value:13},{suit:"clubs",value:1},{suit:"spades",value:2},{suit:"spades",value:3},{suit:"spades",value:4},{suit:"spades",value:5},{suit:"spades",value:6},{suit:"spades",value:7},{suit:"spades",value:8},{suit:"spades",value:9},{suit:"spades",value:10},{suit:"spades",value:11},{suit:"spades",value:12},{suit:"spades",value:13},{suit:"spades",value:1}]
         this.used = new Array()
@@ -28,11 +35,12 @@ class Blackjack{
         this.deckContainer.addEventListener('click', ()=>{
 
             // Caso o usuário esteja apto a puxar cartas, escolhe uma randomicamente do monte e a mostra na mão do jogador
+            this.used.push(this.randomCard())
+            this.counting += this.deck[this.used[this.used.length-1]].value
+            this.remaining.remove(this.used[this.used.length-1])
+            this.showCard(this.handContainer)
 
-            if(this.pickRandomCard()){
-
-                this.showCard(this.handContainer)
-            }
+            if (this.counting >= 21) this.fold = true
 
             // Após meio segundo, a IA faz sua escolha de comprar ou não a carta
 
@@ -47,36 +55,32 @@ class Blackjack{
 
         this.resetContainer.addEventListener('click', ()=>{
             this.fold = true
+            if (this.fold && this.aiFold) this.calcFinal()
             this.aiChoice()
         })
-    }
 
-    calcFinal(){
-        let winner = 0
-        if (this.counting == 21 && this.aiCounting != 21) winner = 'tu'
-        if (this.counting != 21 && this.aiCounting == 21) winner = 'ele'
+        this.betButtons.raise.addEventListener('click', ()=>{
+            this.bet++
+            this.betContainer.innerText = this.bet
+        })
 
-        if (this.counting > 21 && this.aiCounting > 21) {
-            if (this.counting - 21 > this.aiCounting - 21) winner = 'ele'
-            if (this.counting - 21 < this.aiCounting - 21) winner = 'tu'
-        }
+        this.betButtons.lower.addEventListener('click', ()=>{
+            if (this.bet > 1) {
+                this.bet--
+                this.betContainer.innerText = this.bet
+            }
+        })
 
-        if (this.counting < 21 && this.aiCounting < 21) {
-            if (21 - this.counting > 21 - this.aiCounting) winner = 'ele'
-            if (21 - this.counting < 21 - this.aiCounting) winner = 'tu'
-        }
-
-        if (this.counting > 21 && this.aiCounting < 21) winner = 'ele'
-        if (this.counting < 21 && this.aiCounting > 21) winner = 'tu'
-
-        alert(winner)
-        this.resetGame()
-    }
+        this.closeButton.addEventListener('click', ()=>{
+            window.location = '../extras.html'
+        })
+    }   
 
     aiChoice(){
 
-        // Caso a IA
-        if (!this.aiFold && !(this.aiFold >= 21)) {   
+        // Caso a IA não tenha estourado ainda, ela irá verificar suas chances de vitória
+
+        if (!this.aiFold && !(this.aiCounting >= 21)) {   
 
             // Calcula quantos pontos faltam para chegar a 21
             let delta = 21 - this.aiCounting
@@ -93,33 +97,36 @@ class Blackjack{
             // Um random ligeiramente gambiarra para gerar uma margem de erro
             let r = Math.floor(Math.random() * 15)
 
-            // Caso a change de derrota seja maior do que 2/3 e a gambiarra não 'funcione', escolhe uma carta randômica do baralho
+            // Caso a chance de derrota seja maior do que 2/3 e a gambiarra não 'funcione', escolhe uma carta randômica do baralho
 
             if (loss/this.remaining.length < 0.66 || r == 10) {
-                let picked = Math.floor(Math.random() * ((this.deck.length - 1) - 0 + 1)) + 0
-                
-                if (this.used.includes(picked)) this.pickRandomCard()
+                this.used.push(this.randomCard())
+                this.aiCounting += this.deck[this.used[this.used.length-1]].value
+                this.remaining.remove(this.used[this.used.length-1])
+                this.showCard(this.aiHandContainer)
 
-                else{
-                    this.used.push(picked)
-                    this.aiCounting += this.deck[this.used[this.used.length-1]].value
-                    this.showCard(this.aiHandContainer)
+                if (this.aiCounting >= 21) this.aiFold = true
 
-                    
-                    if (this.counting == 21 || this.fold && this.aiCounting < 21) {
-                        this.aiChoice()       
-                    }
+                if (this.fold && this.aiCounting < 21) {
+                    this.aiChoice()       
                 }
             }else{
                 this.aiFold = true
             }
-        }else{
-            console.log(this.counting > 21)
-            if (this.counting > 21) {
-                console.log('cabou')
+            
+            if (this.fold && this.aiFold) {
                 this.calcFinal()
             }
         }
+    }
+
+    randomCard(){
+        let picked
+
+        do picked = Math.floor( Math.random() * ((this.deck.length - 1) - 0 + 1)) + 0 
+        while(this.used.includes(picked)) 
+
+        return picked
     }
 
     showCard(hand){
@@ -128,7 +135,7 @@ class Blackjack{
         let cardHeader = document.createElement('div')
         cardHeader.innerText = this.deck[this.used[this.used.length-1]].value
         let cardCenter = document.createElement('img')
-        cardCenter.src = 'img/'+this.deck[this.used[this.used.length-1]].suit+'.png'
+        cardCenter.src = '../../img/cards/'+this.deck[this.used[this.used.length-1]].suit+'.png'
 
         div.appendChild(cardHeader)
         div.appendChild(cardCenter)
@@ -136,52 +143,55 @@ class Blackjack{
         hand.appendChild(div)
     }
 
-    calcVictory(){
-        if (this.counting == 21){
-            this.totalContainer.style.color = '#d2e459'
-            return 100  
-        } 
-        if (this.counting > 21){
-            this.totalContainer.style.color = '#ff8585'
-            return 0
-        } 
+    calcFinal(){
+        // 0 - Draw
+        // 1 - Win
+        // 2 - Loose
+        let result
 
-        let delta = 21 - this.counting
-        let acertos = 0
+        if (this.counting == 21 && this.aiCounting != 21) result = 1
+        if (this.counting != 21 && this.aiCounting == 21) result = 2
 
-        for (let i = 0; i < this.remaining.length; i++) {
-            if (this.remaining[i].value == delta) acertos++
+        if (this.counting > 21 && this.aiCounting > 21) {
+            if (this.counting - 21 > this.aiCounting - 21) result = 2
+            if (this.counting - 21 < this.aiCounting - 21) result = 1
         }
-    
-        return (acertos/this.remaining.length) * 100
 
-    }
-
-    pickRandomCard(){
-        if (this.counting >= 21){
-            this.fold = true
-            return false
-        } 
-
-        if (this.used.length == 52) return false
-        
-        let picked = Math.floor(Math.random() * ((this.deck.length - 1) - 0 + 1)) + 0
-        
-        if (this.used.includes(picked)) this.pickRandomCard()
-        
-        else{
-            this.used.push(picked)
-            this.counting += this.deck[this.used[this.used.length-1]].value
-            this.remaining.remove(this.used[this.used.length-1])
-            return picked
+        if (this.counting < 21 && this.aiCounting < 21) {
+            if (21 - this.counting > 21 - this.aiCounting) result = 2
+            if (21 - this.counting < 21 - this.aiCounting) result = 1
         }
+
+        if (this.counting > 21 && this.aiCounting < 21) result = 2
+        if (this.counting < 21 && this.aiCounting > 21) result = 1
+
+        if (this.counting == this.aiCounting) result = 0
+
+        if (result == 1) {
+            c_user.addCoin(this.bet*2)
+            let c = swal({
+                title: 'Você ganhou!',
+                text: '+ '+this.bet*2  +' moedinhas',
+                buttons: false,
+                timer:600
+            })
+        }else if(result == 2){
+            c_user.addCoin(this.bet*-1)
+            let c = swal({
+                title: 'Você perdeu :(',
+                text: '- '+this.bet  +' moedinhas',
+                buttons: false,
+                timer:600
+            })
+        }
+
+        this.resetGame()
     }
 
     resetGame(){
         this.handContainer.innerHTML = ''
         this.aiHandContainer.innerHTML = ''
-        this.remaining = [{suit:"hearts",value:2},{suit:"hearts",value:3},{suit:"hearts",value:4},{suit:"hearts",value:5},{suit:"hearts",value:6},{suit:"hearts",value:7},{suit:"hearts",value:8},{suit:"hearts",value:9},{suit:"hearts",value:10},{suit:"hearts",value:11},{suit:"hearts",value:12},{suit:"hearts",value:13},{suit:"hearts",value:1},{suit:"diamonds",value:2},{suit:"diamonds",value:3},{suit:"diamonds",value:4},{suit:"diamonds",value:5},{suit:"diamonds",value:6},{suit:"diamonds",value:7},{suit:"diamonds",value:8},{suit:"diamonds",value:9},{suit:"diamonds",value:10},{suit:"diamonds",value:11},{suit:"diamonds",value:12},{suit:"diamonds",value:13},{suit:"diamonds",value:1},{suit:"clubs",value:2},{suit:"clubs",value:3},{suit:"clubs",value:4},{suit:"clubs",value:5},{suit:"clubs",value:6},{suit:"clubs",value:7},{suit:"clubs",value:8},{suit:"clubs",value:9},{suit:"clubs",value:10},{suit:"clubs",value:11},{suit:"clubs",value:12},{suit:"clubs",value:13},{suit:"clubs",value:1},{suit:"spades",value:2},{suit:"spades",value:3},{suit:"spades",value:4},{suit:"spades",value:5},{suit:"spades",value:6},{suit:"spades",value:7},{suit:"spades",value:8},{suit:"spades",value:9},{suit:"spades",value:10},{suit:"spades",value:11},{suit:"spades",value:12},{suit:"spades",value:13},{suit:"spades",value:1}]
-        if (this.used.length > 40) {
+        if (this.used.length > 30) {
             this.used = new Array()
         }
         this.counting = 0
